36831d15ef2d38f04985d26b526f97ea9754e74e
TevoBaudejas <75721196+TevoBaudejas@users.noreply.github.com>
2020-12-31 09:45:50 -0300

    Add files via upload

---
+++ voicecreate.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import traceback
+import sys
+
+bot = commands.Bot(command_prefix=".")
+
+bot.remove_command("help")
+
+DISCORD_TOKEN = 'NzkzNTY3NzA0NzAzMTcyNjI5.X-uJhA.Mx2XGZAtmcm_yxsOLLqUyeul9sY'
+
+initial_extensions = ['cogs.voice']
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+bot.run(DISCORD_TOKEN)
